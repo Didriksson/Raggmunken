19047b1d11d573f7c0ee6ee8990bbb0beb3d8d5a
Mattias Didriksson <mattias.didriksson@gmail.com>
2015-12-13 18:50:14 +0100

    Settinsg för postgresql

--- Raggmunken/settings.py
+++ Raggmunken/settings.py
@@ -86,8 +86,13 @@ WSGI_APPLICATION = 'Raggmunken.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'postgres',                      # Or path to database file if using sqlite3.
+         # The following settings are not used with sqlite3:
+        'USER': 'postgres',
+        'PASSWORD': 'mysecretpassword',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
